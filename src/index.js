86adb8bb1caa5c6c546f26b79b08a59881010b92
lixiaolongxl <lxldevtest@163.com>
2021-12-15 22:28:32 +0800

    up

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 // import React from 'react';
 import ReactDOM from 'react-dom';
 import React, { useRef, useState ,useEffect} from 'react'
-import { Canvas, useFrame,useThree } from '@react-three/fiber'
+import { Canvas, useFrame,useThree,extend } from '@react-three/fiber'
 import './index.css';
 
 import reportWebVitals from './reportWebVitals';
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js'
+extend({OrbitControls})
+
+const Orbit = ()=>{
+  const {camera,gl} = useThree()
+  return (
+    <orbitControls args={[camera,gl.domElement]}/>
+  )
+}
 function Box(props) {
   const {gl,camera,scene} = useThree()
   useEffect(()=>{
@@ -18,7 +27,7 @@ function Box(props) {
   const [clicked, click] = useState(false)
   // Subscribe this component to the render-loop, rotate the mesh every frame
   useFrame((state, delta) => {
-    // console.log(state,delta);
+    // console.log(ref.current);
     
     ref.current.rotation.x += 0.01
     ref.current.rotation.y += 0.01
@@ -40,16 +49,30 @@ function Box(props) {
 
 ReactDOM.render(
   <div style={{width: '100%', height: '100vh'}}>
-    <Canvas style={{backgroundColor: 'black'}}>
+    <Canvas style={{backgroundColor: 'black'}} camera={{position:[3,3,3]}}>
+      <Orbit/>
       <ambientLight intensity={0.1}/>
       <pointLight position={[10, 10, 10]} />
       <directionalLight color="red" position={[0, 0, 5]} />
       <Box position={[-1.2, 0, 0]} />
-      <Box position={[1.2, 0, 0]} />
-      <mesh position={[0,0,0]}>
+      {/* <Box position={[1.2, 0, 0]} /> */}
+      <axesHelper args={[5]}/>
+      {/* <mesh position={[0,0,0]}>
         <boxGeometry/>
         <meshBasicMaterial color="red"/>
-      </mesh>
+      </mesh> */}
+      {/* 点材质 */}
+      {/* <points>
+        <boxGeometry args={[1,1,1]}></boxGeometry>
+        <pointsMaterial color='0xff0000' size={0.5}></pointsMaterial>
+      </points> */}
+      {/* 线材质 */}
+      <line>
+        <boxGeometry args={[1,1,1]}></boxGeometry>
+        <pointsMaterial color='0xff0000' size={0.5}></pointsMaterial>
+      </line>
+      {/* 面材质 */}
+      
   </Canvas>
   </div>,
   document.getElementById('root')
