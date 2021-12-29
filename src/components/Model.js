7086c4c14595f46d73970b598ce81c4c20aacd60
lixiaolongxl <lxldevtest@163.com>
2021-12-29 20:32:10 +0800

    up

--- src/components/Model.js
+++ src/components/Model.js
@@ -4,12 +4,8 @@ import * as THREE from 'three'
 const Model = props=>{
     const model = useLoader(GLTFLoader,props.path)
 
-
-
-    console.log(props.path,model)
-
     if(model.animations.length>0){
-        
+        console.log(props.path,model)
     }
     model.scene.traverse(child=>{
         if(child.isMesh){
